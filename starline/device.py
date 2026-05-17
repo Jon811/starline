d9a9421e597f7f8669dc6123c820a31a6b642a2b
Jon <58070934+Jon811@users.noreply.github.com>
2026-05-17 20:52:36 +0300

    Update device.py
    adf

--- starline/device.py
+++ starline/device.py
@@ -9,6 +9,8 @@ from .const import (
     DEVICE_FUNCTION_STATE,
 )
 from datetime import datetime
+from logging import logging
+_LOGGER = logging.getLogger(__name__)
 
 class StarlineDevice:
     """StarLine device class."""
